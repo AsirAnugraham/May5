fabd473fe4d8416e9a2d9ce3233c529533333576
AsirAnugraham <97786719+AsirAnugraham@users.noreply.github.com>
2023-05-05 18:06:50 +0530

    Add files via upload

---
+++ Hash.java
@@ -0,0 +1,15 @@
+package test;
+import java.util.HashSet;
+
+public class Hash {
+  public static void main(String[] args) {
+    HashSet<String> cars = new HashSet<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("BMW");
+    cars.add("Mazda");
+    System.out.println(cars);
+  }
+}
+//Inspite BMW was added two times in hash set we only get one value returned
---
+++ TypeCasting.java
@@ -0,0 +1,11 @@
+package test;
+
+public class TypeCasting {
+  public static void main(String[] args) {
+    double myDouble = 9.78d;
+    int myInt = (int) myDouble; 
+
+    System.out.println(myDouble);
+    System.out.println(myInt);
+  }
+}
